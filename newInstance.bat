0b65278bbccda6497a23c2f8a7bf01e884386be8
VelaLaura30 <laura.vela03@uptc.edu.co>
2024-03-21 19:50:26 -0500

    okkkkkkk

--- newInstance.bat
+++ newInstance.bat
@@ -1,13 +1,11 @@
 @echo off
 
-
 REM Leer la variable de entorno del sistema
 echo La ruta del Dockerfile es: %DOCKERFILE_DIR%
 
-
 REM Verifica si existe el archivo que contiene el valor del contador
 if not exist counter.txt (
-    echo > counter.txt
+    echo. > counter.txt
 )
 
 REM Lee el valor del contador desde el archivo
@@ -26,17 +24,13 @@ REM Incrementa el contador para la próxima ejecución
 set /A CONTADOR+=1
 
 REM Guarda el nuevo valor del contador en el archivo
-echo %CONTADOR% > contador.txt
+echo %CONTADOR% > counter.txt
 
 REM Puerto de mapeo del contenedor (puerto del host:puerto del contenedor)
 set PUERTO_MAPEADO=%PORT%:%PORT%
 
-
 REM Construye y ejecuta el comando Docker
-
 docker build -t %IMAGEN% --build-arg PORT=%PORT% -f %DOCKERFILE_DIR%\Dockerfile %DOCKERFILE_DIR%
-
-docker run -d -p %PUERTO_MAPEADO% --name %CONTENEDOR% -e DATABASE_URL -e ID_SERVICE=server%CONTADOR% -e NODE_SERVICE_IP=172.17.0.%CONTADOR% -e NODE_SERVICE_PORT=%PORT% -e NODE_VERSION=18.19.1 -e PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -e YARN_VERSION=1.22.19 %IMAGEN%
-
+docker run -d -p %PUERTO_MAPEADO% --name %CONTENEDOR% -e DATABASE_URL -e ID_SERVICE -e NODE_SERVICE_IP -e NODE_SERVICE_PORT -e NODE_VERSION -e PATH -e YARN_VERSION %IMAGEN%
 
 echo Contenedor creado exitosamente con nombre de imagen: %IMAGEN% y puerto mapeado: %PUERTO_MAPEADO%.
